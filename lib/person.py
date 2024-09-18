9a9f4d872c129618df568f7316be34dbf5ebe855
victorndungi <victor.ndungi@student.moringaschool.com>
2024-09-18 11:04:25 +0300

    Complete

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,12 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
+
+Victor = Person()
+Victor.talk()
+Victor.walk()
